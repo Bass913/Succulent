adce1377664072a7cefc5d20628519da3e15aa8a
Bass913 <bastiendikiadi@outlook.fr>
2023-12-29 00:53:20 +0100

    Login almost done

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -2,13 +2,16 @@ import User from 'App/Models/User'
 import Hash from '@ioc:Adonis/Core/Hash'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import RegisterValidator from 'App/Validators/RegisterValidator'
+import Env from '@ioc:Adonis/Core/Env'
+import crypto = require('crypto');
+
 
 export default class AuthController {
 
     public async register({ request, response }: HttpContextContract) {
-
+        // Validate request
         await request.validate(RegisterValidator);
-
+        // request body
         const user = new User();
         const email = request.input('email');
         const password = request.input('password');
@@ -16,35 +19,45 @@ export default class AuthController {
         const lastname = request.input('lastname');
         const dob = request.input('dob');
 
+        // User creation
         await user.fill({ email: email, password: password, firstname: firstname, lastname: lastname, dob: dob }).save();
 
-        response.status(201);
+        response.created();
     }
 
-    public async login({ auth, request, response }: HttpContextContract) {
+    public async login({ auth, request, response, session }: HttpContextContract) {
+        // request body
         const email = request.input('email')
         const password = request.input('password')
 
         // Lookup user manually
         const user = await User.findByOrFail('email', email);
-
         // Verify password
         if (!(await Hash.verify(user.password, password))) {
             return response.unauthorized({ error: 'Identifiants invalides, email ou mot de passe incorrect.' })
         }
-
         // Verify status
         if (!user.active) {
             return response.forbidden({ error: 'Votre compte n\'est pas activé. Veuillez consulter vos mails.' })
         }
         // Generate token
-        await auth.use('api').generate(user, {
+        const token = await auth.use('api').generate(user, {
             expiresIn: '30 days'
         });
+        const cookieName = Env.get('SUCCULENT_AUTH_TOKEN').concat(crypto.randomBytes(6).toString('hex'));
+
+        session.put('cookieName', cookieName)
+        response.cookie(cookieName, token.toJSON().token, {
+            maxAge: "30d"
+        })
+
+        return token
     }
 
-    public async logout({ auth }: HttpContextContract) {
+    public async logout({ auth, session, response }: HttpContextContract) {
         await auth.use('api').revoke()
+
+        response.clearCookie(session.get('cookieName'))
         return {
             revoked: true
         }
